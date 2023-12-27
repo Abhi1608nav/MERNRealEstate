8975a561c7c24944b1b17fb73d133ea0614b284e
Abhi1608nav <abhinavshakya1608@gmail.com>
2023-12-28 00:19:53 +0530

    Created a Searchpage

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
 import Listing from './pages/Listing';
+import Search from './pages/Search';
 
 
 function App() {
@@ -25,7 +26,7 @@ function App() {
         <Route path='/sign-in' element={<SigIn/>} />
         <Route path='/sign-up' element={<SignUp/>} />
         <Route path='/listing/:listingId' element={<Listing/>} />
-
+         <Route path='/search' element={<Search/>} />
         <Route element={<PrivateRoute/>}>
         <Route path='/profile' element={<Profile/>} />
         <Route path='/create-listing' element={<CreateListing/>} />
